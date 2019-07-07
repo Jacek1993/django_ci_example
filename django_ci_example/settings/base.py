973fb95f3f10f8831d2726e40cce11c05567002a
Jacek1993 <32387209+Jacek1993@users.noreply.github.com>
2019-07-07 23:29:41 +0200

    Update base.py
    make it runnable

--- django_ci_example/settings/base.py
+++ django_ci_example/settings/base.py
@@ -1,14 +1,4 @@
-"""
-Django settings for django_ci_example project.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
